b63280677b8a4414fbdf13b6b784027a06fec7fd
Tarinder Uppal <tarinderu@gmail.com>
2022-02-04 15:52:35 -0800

    init

--- [Assignment2-1]Program2-10.cpp
+++ [Assignment2-1]Program2-10.cpp
@@ -3,8 +3,17 @@ using namespace std;
 
 int main()
 {
-    cout << "We have made a long journey of 4276 miles. ";
-    cout << "Our checking account balance is -20. ";
-    cout << "About 189000 days ago Columbus stood on this spot.";
+    int checking;
+    unsigned int miles;
+    long;
+    
+    checking = -20;
+    miles = 4276;
+    long ago = 189000;
+    cout << "We have made a long journey of " << miles;
+    cout << " miles.\n";
+    cout << "Our checking account balance is. " << checking;
+    cout << "\n About " << ago;
+    cout << " Columbus stood on this spot.\n";
     return 0;
 }
