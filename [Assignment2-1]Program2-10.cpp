a0eda52bda0e61fad7fc5be873f3cfd294ea80c9
Tarinder Uppal <tarinderu@gmail.com>
2022-02-04 14:41:15 -0800

    init

---
+++ [Assignment2-1]Program2-10.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "We have made a long journey of 4276 miles. ";
+    cout << "Our checking account balance is -20. ";
+    cout << "About 189000 days ago Columbus stood on this spot.";
+    return 0;
+}
