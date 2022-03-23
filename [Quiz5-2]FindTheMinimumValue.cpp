27840871062531d5b85f86b34cabe5d6aa6a91d4
tarinderu <98419859+tarinderu@users.noreply.github.com>
2022-03-22 21:39:54 -0700

    Update [Quiz5-2]FindTheMinimumValue.cpp

--- [Quiz5-2]FindTheMinimumValue.cpp
+++ [Quiz5-2]FindTheMinimumValue.cpp
@@ -1,14 +1,24 @@
 #include <iostream>
-#include <ctime>
-#include <cstdlib>
 using namespace std;
+
 int main()
 {
-    srand((unsigned) time(0));
-    int randomNumber;
-    for (int index = 0; index < 5; index ++){
-    
-        randomNumber = (rand() % 100) + 1;
-        cout << randomNumber << endl;
+    int input[100], cout, i, min;
+    cout << "Enter how many numbers there are\n";
+    cin >> count;
+    cout << "Enter" << count << " numbers \n";
+    for (i = 0; i < count; i++_)
+    {
+        cin >> input[i];
     }
+    min = input [0];
+    for(i=0; i < count; i++)
+    {
+        if(input[i] < min)
+        {
+            min = input[i];
+        }
+    }
+    cout << "Minimum value\n" << min;
+    return 0;
 }
